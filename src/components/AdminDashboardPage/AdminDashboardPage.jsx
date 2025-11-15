7845bd254c8669a0b8c71e5b1ba00359dc3db7ae
alaalshehabii <alaa.hsan.hajj@gmail.com>
2025-11-15 19:12:25 +0300

    add report loading logic and updatestatus function

--- src/components/AdminDashboardPage/AdminDashboardPage.jsx
+++ src/components/AdminDashboardPage/AdminDashboardPage.jsx
@@ -4,3 +4,20 @@ import * as reportService from "../../services/reportService";
 const AdminDashboardPage = () => {
   const [reports, setReports] = useState([]);
   const [filter, setFilter] = useState("all");
+
+
+  useEffect(() => {
+    async function loadReports() {
+      const data = await reportService.index();
+      setReports(data);
+    }
+    loadReports();
+  }, []);
+
+  async function updateStatus(id, status) {
+    await reportService.update(JSON.stringify({ status }), id);
+
+    setReports((prev) =>
+      prev.map((r) => (r._id === id ? { ...r, status } : r))
+    );
+  }
