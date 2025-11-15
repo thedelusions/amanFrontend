72477ba543ccb6b7a75eaeb359430a5c473e4f14
alaalshehabii <alaa.hsan.hajj@gmail.com>
2025-11-15 19:00:28 +0300

    add report loading logic and form input handler

--- src/components/UserProfilePage/UserProfilePage.jsx
+++ src/components/UserProfilePage/UserProfilePage.jsx
@@ -15,3 +15,19 @@ const UserProfilePage = () => {
     phone: user?.phone || "",
     area: user?.area || "",
   });
+    useEffect(() => {
+    async function loadReports() {
+      const allReports = await reportService.index();
+
+      const mine = allReports.filter(
+        (r) => r.author && r.author._id === userId
+      );
+
+      setReports(mine);
+    }
+
+    if (userId) loadReports();
+  }, [userId]);
+
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
