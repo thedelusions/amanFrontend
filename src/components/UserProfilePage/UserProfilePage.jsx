e697e9726afa478b73055c9f048c6a3a6bbbc03a
alaalshehabii <alaa.hsan.hajj@gmail.com>
2025-11-15 18:59:08 +0300

    setup component state, extract userId, and add formData defaults

--- src/components/UserProfilePage/UserProfilePage.jsx
+++ src/components/UserProfilePage/UserProfilePage.jsx
@@ -3,3 +3,15 @@ import { UserContext } from "../../contexts/UserContext";
 
 import * as reportService from "../../services/reportService";
 import * as userService from "../../services/userService";
+
+const UserProfilePage = () => {
+  const { user } = useContext(UserContext);
+  const userId = user?._id;
+
+  const [reports, setReports] = useState([]);
+
+  const [formData, setFormData] = useState({
+    name: user?.name || "",
+    phone: user?.phone || "",
+    area: user?.area || "",
+  });
